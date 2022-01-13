89b0d209e7e9b463cd9fd3a527652832af69c69b
zihansu5 <zsu5@ualberta.ca>
2022-01-12 21:19:32 -0700

    more details

--- lab1.py
+++ lab1.py
@@ -1,4 +1,8 @@
 import requests
+import sys
 url = "https://raw.githubusercontent.com/zihansu5/CMPUT404lab1/master/lab1.py"
 reponse = requests.get(url)
+print("requests version:", requests.__version__)
+print("python version:", sys.version)
+print("contents:")
 print(reponse.text)
